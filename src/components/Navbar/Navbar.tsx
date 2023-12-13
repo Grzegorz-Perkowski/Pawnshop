ceb4bd685ebcac80afbd7d5d8777ce282d8eaccc
Grzegorz-Perkowski <58942682+Grzegorz-Perkowski@users.noreply.github.com>
2023-12-13 11:31:15 +0100

    Add total quantity count to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { CartItem as ICartItem } from "../../interfaces/Cart.interface";
 
 function Navbar() {
+  const cart = useSelector((state: { cart: ICartItem[] }) => state.cart);
+
+  const totalQuantity = cart.reduce(
+    (accumulator, currentItem) => accumulator + currentItem.quantity,
+    0
+  );
+
   return (
     <div>
       <Link to="/">Home</Link>
       <br />
       <Link to="/cart">Cart</Link>
+      {totalQuantity > 0 ? <span>({totalQuantity})</span> : <span>(0)</span>}
     </div>
   );
 }
