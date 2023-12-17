2747124f015e07c5b562dfe8a392d97a791f4c0a
Grzegorz-Perkowski <58942682+Grzegorz-Perkowski@users.noreply.github.com>
2023-12-17 16:10:23 +0100

    Add styles to product details component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,4 +1,8 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 
--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -2,6 +2,10 @@ import { useGetProductByIdQuery } from "../../features/api/apiSlice";
 import { useParams } from "react-router-dom";
 import { IProduct } from "products-types";
 import CircularLoader from "../CircularProgress/CircularProgress";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import styled from "@emotion/styled";
 
 const ProductDetails = () => {
   const { id } = useParams<{ id?: string }>();
@@ -23,21 +27,38 @@ const ProductDetails = () => {
     return <div>No data available</div>;
   }
 
-  const { title, price, description, category, image, rating }: IProduct =
-    product;
+  const { title, description, image }: IProduct = product;
 
   return (
-    <div>
-      <p>Product details</p>
-      <img src={image} alt={title} />
-      <p>{title}</p>
-      <p>{price}</p>
-      <p>{description}</p>
-      <p>{category}</p>
-      <p>{rating.rate}</p>
-      <p>{rating.count}</p>
-    </div>
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", gap: 4 }}
+    >
+      <Image src={image} alt={title} />
+      <Box
+        sx={{
+          width: "50%",
+          display: "flex",
+          flexDirection: "column",
+          gap: 4,
+        }}
+      >
+        <Typography variant="h4" color="primary">
+          {title}
+        </Typography>
+        <Typography variant="body1">{description}</Typography>
+      </Box>
+    </Grid>
   );
 };
 
 export default ProductDetails;
+
+const Image = styled.img`
+  width: 30%;
+  height: 400px;
+  object-fit: contain;
+`;
